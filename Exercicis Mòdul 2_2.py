07b053ffb7736c98fc8f11eb0aad51d7feb46c4d
Joan Palahí <JoanbPalahi@users.noreply.github.com>
2023-11-13 23:52:07 +0100

    Add files via upload

---
+++ Exercicis Mòdul 2_2.py
@@ -0,0 +1,7 @@
+trimestres = [
+    ['Gener', 'Febrer', 'Març'],
+    ['Abril', 'Maig', 'Juny'],
+    ['Juliol', 'Agost', 'Setembre'],
+    ['Octubre', 'Novembre', 'Desembre']
+]
+print(trimestres[0][1])
