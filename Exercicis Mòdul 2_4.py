4ef053595631d0f478fae6e3e42168f642a9d2f2
Joan Palahí <JoanbPalahi@users.noreply.github.com>
2023-11-14 13:29:18 +0100

    Add files via upload

---
+++ Exercicis Mòdul 2_4.py
@@ -0,0 +1,11 @@
+trimestres = [
+    ['Gener', 'Febrer', 'Març'],
+    ['Abril', 'Maig', 'Juny'],
+    ['Juliol', 'Agost', 'Setembre'],
+    ['Octubre', 'Novembre', 'Desembre']
+]
+setembre_t2 = trimestres[2][2] 
+octubre_t3 = trimestres[3][0] 
+
+print(setembre_t2)
+print(octubre_t3)
